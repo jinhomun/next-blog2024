2fbaf4dfc79464c82aacde9df8992e8acb46c882
jinhomun <144635699+jinhomun@users.noreply.github.com>
2024-01-23 23:34:58 +0900

    집

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
-import Theme from '../theme/Theme'
-import Menu from '../menu/Menu'
+import Link from "next/link";
+import React from "react";
+import Theme from "../theme/Theme";
+import Menu from "../menu/Menu";
 
 const Header = () => {
-    return (
-        <header id='header' role='banner'>
-            <div className='left'>
-                <h1 className='logo'>
-                    <Link href="/"> Jins blog</Link>
-                </h1>
-                <Theme />
-            </div>
-            <nav className='nav'>
+  return (
+    <header id="header" role="banner">
+      <div className="left">
+        <h1 className="logo">
+          <Link href="/"> Jins blog</Link>
+        </h1>
+        <Theme />
+      </div>
+      <nav className="nav">
+        <Link href="/">
+          <img src="/home.png" alt="" />
+        </Link>
+        <Link href="/blog">
+          <img src="/blog.png" alt="" />
+        </Link>
+        <Link href="/notice">
+          <img src="/notice.png" alt="" />
+        </Link>
+      </nav>
+      <div className="right">
+        <Menu />
+      </div>
+    </header>
+  );
+};
 
-                <Link href='/'><img src="/home.png" alt="" /></Link>
-                <Link href='/blog'><img src="/note.png" alt="" /></Link>
-                <Link href='/notice'><img src="/home.png" alt="" /></Link>
-
-            </nav>
-            <div className='right'>
-                <Menu />
-            </div>
-        </header >
-    )
-}
-
-export default Header
+export default Header;
